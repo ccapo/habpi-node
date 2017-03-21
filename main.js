7830af4003d7f376fbede5453145fc681c40f130
Christopher Capobianco <ccapo@users.noreply.github.com>
2017-03-21 01:33:49 -0400

    Add authentication check for sqlite DB

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const HABPi = require('./lib/habpi').HABPi;
 
 // HABPi API
 let habpi = new HABPi();
+
+// Check if we are authenicated
+habpi._sequelize.authenticate().then(() => {
+	habpi.log('Connection has been established successfully', 'alert');
+}).catch(err => {
+	habpi.log(`Unable to connect to the database: ${JSON.stringif(err)}`, 'error');
+});
