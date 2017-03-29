e7f2fb3262245f767d652d829c0342b0d8ca92bf
Christopher Capobianco <ccapo@users.noreply.github.com>
2017-03-29 01:24:21 -0400

    Update sql query to extract results, update .gitignore and reduce logging

--- db/sql/message_query.sql
+++ db/sql/message_query.sql
@@ -1,8 +1,14 @@
 .mode csv message;
-.output db/temp.dat
-SELECT m.created_at,m.message FROM message AS m JOIN message_type AS mt ON m.message_type_id = mt.id WHERE mt.name = "TEMP";
-.output db/baro.dat
-SELECT m.created_at,m.message FROM message AS m JOIN message_type AS mt ON m.message_type_id = mt.id WHERE mt.name = "BARO";
-.output db/baro_alt.dat
-SELECT m.created_at,m.message FROM message AS m JOIN message_type AS mt ON m.message_type_id = mt.id WHERE mt.name = "BARO_ALT";
+.output bmp180_temp.dat
+SELECT m.created_at,m.message FROM message AS m JOIN message_type AS mt ON m.message_type_id = mt.id JOIN sensor_type AS st ON m.sensor_type_id = st.id WHERE st.name = "BMP180" AND mt.name = "TEMP";
+.output bmp180_pres.dat
+SELECT m.created_at,m.message FROM message AS m JOIN message_type AS mt ON m.message_type_id = mt.id JOIN sensor_type AS st ON m.sensor_type_id = st.id WHERE st.name = "BMP180" AND mt.name = "PRES";
+.output bmp180_alt.dat
+SELECT m.created_at,m.message FROM message AS m JOIN message_type AS mt ON m.message_type_id = mt.id JOIN sensor_type AS st ON m.sensor_type_id = st.id WHERE st.name = "BMP180" AND mt.name = "ALT";
+.output mpl3115a2_temp.dat
+SELECT m.created_at,m.message FROM message AS m JOIN message_type AS mt ON m.message_type_id = mt.id JOIN sensor_type AS st ON m.sensor_type_id = st.id WHERE st.name = "MPL3115A2" AND mt.name = "TEMP";
+.output mpl3115a2_pres.dat
+SELECT m.created_at,m.message FROM message AS m JOIN message_type AS mt ON m.message_type_id = mt.id JOIN sensor_type AS st ON m.sensor_type_id = st.id WHERE st.name = "MPL3115A2" AND mt.name = "PRES";
+.output mpl3115a2_alt.dat
+SELECT m.created_at,m.message FROM message AS m JOIN message_type AS mt ON m.message_type_id = mt.id JOIN sensor_type AS st ON m.sensor_type_id = st.id WHERE st.name = "MPL3115A2" AND mt.name = "ALT";
 .quit
