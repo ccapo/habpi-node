02113f86b2216e6afeaae268e8f52edcd9044daa
Christopher Capobianco <ccapo@users.noreply.github.com>
2017-04-03 22:51:06 -0400

    Removed DB backup, and updated output SQL query

--- db/sql/message_query.sql
+++ db/sql/message_query.sql
@@ -1,4 +1,4 @@
-.mode csv message;
+.mode tabs message;
 .output bmp180_temp.dat
 SELECT m.created_at,m.message FROM message AS m JOIN message_type AS mt ON m.message_type_id = mt.id JOIN sensor_type AS st ON m.sensor_type_id = st.id WHERE st.name = "BMP180" AND mt.name = "TEMP";
 .output bmp180_pres.dat
